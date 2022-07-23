71645666f818d4b5f0c89e40a67053abefd36576
palwashamalik <palwasha.malik@devsinc.com>
2022-07-24 03:52:49 +0500

    Sign Up Page Functionality
    * Created User model
    * Added Profile Picture change/add feature
    * Nested attributes for location and items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit( :name, :email, :password,:age,:gender,:infected,:avatar ,items_attributes: [:id,:points,:itemname,:quantity,:_destroy],locations_attributes: [:latitude,:longitude])}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:avatar,locations_attributes: [:id,:latitude,:longitude])}
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :locations
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   root 'home#index'
 end
