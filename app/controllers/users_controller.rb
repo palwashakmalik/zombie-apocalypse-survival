740c54622a42ee98d0a47a188f6c8fd9c60eff38
palwashamalik <palwasha.malik@devsinc.com>
2022-07-26 00:00:37 +0500

    Gem as_votable Used
    * Inventory of all users accessible
    * votable gem to flag user as infected

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :find_user
+
+  def upvote
+    @user = User.find(params[:id])
+    @user.upvote_from current_user if @user.get_upvotes.size < 5
+    byebug
+    if @user.get_upvotes.size == 5
+      @user.infected = true
+    end
+    redirect_to root_path
+  end
+  def show
+    @items = @user.items
+  end
+
+  def downvote
+    @user = User.find(params[:id])
+    @user.downvote_from current_user unless @user.infected
+    redirect_to root_path
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by(user_params)
+  end
+
+  def user_params
+    params.permit(:id)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,14 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => {:registrations => "registrations"}
   root 'home#index'
+  resources :users do
+    member do
+      put "like"=> "users#upvote"
+      put "unlike"=> "users#downvote"
+    end
+  end
+
+  resources :users do
+  resources :items
+  end
 end
