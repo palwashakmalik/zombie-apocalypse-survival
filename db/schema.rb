ac07768b3f3e0415cd35171d85e88e0aead8b974
palwashamalik <palwasha.malik@devsinc.com>
2022-07-26 12:08:54 +0500

    Removed quantity table and corrected formatting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_25_121613) do
+ActiveRecord::Schema.define(version: 2022_07_26_070257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2022_07_25_121613) do
 
   create_table "items", force: :cascade do |t|
     t.string "itemname"
-    t.integer "quantity"
     t.integer "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
