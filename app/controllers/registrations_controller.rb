c1e0ae5a67df4a32bbe64426a3629a026052597a
palwashamalik <palwasha.malik@devsinc.com>
2022-07-26 22:17:24 +0500

    added registration controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class RegistrationsController < Devise::RegistrationsController
+  def edit
+    @location = resource.locations.new
+    render :edit
+  end
+
+  def update_resource(resource, params)
+    resource.update(params)
+  end
+end
