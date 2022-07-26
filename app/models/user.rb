2afa7a18362e6e024ba9df17f71c902cc98dd09a
palwashamalik <palwasha.malik@devsinc.com>
2022-07-26 22:19:44 +0500

    added user  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ApplicationRecord
          :validatable, :rememberable
 
   has_one_attached :avatar
+  has_many :locations, inverse_of: :user
+  has_many :items, inverse_of: :user
+  accepts_nested_attributes_for :items, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :locations
 
   enum gender: { female: 1, male: 2, other: 3 }
+  def with_item_and_location
+    items.new if locations.blank?
+    locations.new if locations.blank?
+    self
+  end
 end
