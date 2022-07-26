d326ba21a3f656e5c9c6c666d5454803269bb443
palwashamalik <palwasha.malik@devsinc.com>
2022-07-26 15:42:40 +0500

    fixed pr changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 <<<<<<< HEAD
+<<<<<<< HEAD
   def index
     @users = User.all_users_accept_current(current_user)
   end
@@ -9,4 +10,9 @@ def index
 end
 
 >>>>>>> 7ef9055 (Gem as_votable Used)
+=======
+  def index
+    @users = User.where.not(id: current_user.id) if current_user
+  end
+>>>>>>> f98ea3a (fixed pr changes)
 end
