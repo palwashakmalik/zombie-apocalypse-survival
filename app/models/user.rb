987bfd39b057d53200aff50c3212875d60f4daac
palwashamalik <palwasha.malik@devsinc.com>
2022-08-02 13:33:43 +0500

    added dblevel validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,11 @@ class User < ApplicationRecord
   enum gender: { female: 1, male: 2, other: 3 }
 
   has_one_attached :avatar
-  has_many :locations, inverse_of: :user, dependent: :delete_all
   has_many :items, inverse_of: :user, dependent: :delete_all
   acts_as_votable
   acts_as_voter
 
   accepts_nested_attributes_for :items, allow_destroy: true, reject_if: :all_blank
-  accepts_nested_attributes_for :locations
 
   validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
