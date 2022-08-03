7dffc61982a3913bcf3efdbc7c47618e3b2471be
palwashamalik <palwasha.malik@devsinc.com>
2022-08-03 16:44:19 +0500

    add data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(admin: true, email: 'palwasha@gmail.com', name: 'palwasha', age: 18, gender: 'female')
+User.create(admin: false, email: 'palwashamalik@gmail.com', name: 'palwashamalik', age: 23, gender: 'female', infected: false,
+            location_history: { 'latitude' => '2', 'longitude' => '54' }, items_attributes: [{item_name: 'water', points: 12}])
+User.create(admin: false, email: 'nimra@gmail.com', name: 'nimra', age: 18, gender: 'female', infected: false,
+            location_history: { 'latitude' => '65', 'longitude' => '445' }, items_attributes: [{item_name: 'wheat', points: 24}])
