108ad8ea4a0f99c814961aa58b4ec8d03cf464b0
palwashamalik <palwasha.malik@devsinc.com>
2022-08-04 15:52:37 +0500

    cloudinary file in initializer

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'palwashakhanmalik'
+  config.api_key =  '467973776213517'
+  config.api_secret =  'kzqEUU_yZan2ILr7siE1KLA_dSg'
+  config.api_secret =  true
+end
