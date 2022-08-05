e868891c858265127e91db0bc96f1bd50ff19a13
palwashamalik <palwasha.malik@devsinc.com>
2022-08-05 16:05:18 +0500

    heroku asset precompilation and setting

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
   skip_before_action :authenticate_user!
   def update_resource(resource, params)
     if @user.location_history
-      locations = [] << @user.location_history < params[:location_history]
+      locations = [] << @user.location_history << params[:location_history]
       params[:location_history] = locations.flatten
     end
     resource.update(params)
