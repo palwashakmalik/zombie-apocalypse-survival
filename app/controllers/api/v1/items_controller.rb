5df60fcf26c6b9457e9439a315c400b90521aadd
palwashamalik <palwasha.malik@devsinc.com>
2022-08-24 21:47:31 +0500

    requested changes made

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,10 +1,13 @@
 module Api::V1
   class ItemsController < ApplicationController
+    skip_before_action :authenticate_user!
     before_action :set_user, only: :index
     def index
       @items = @user.items
       render json: @items
     end
+
+    private
 
     def set_user
       @user = User.find_by(id: params[:user_id])
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,8 +2,10 @@
 
 module Api::V1
   class UsersController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:index]
     def index
       @users = User.all
+
       render json: @users
     end
   end
